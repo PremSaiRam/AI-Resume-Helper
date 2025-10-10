08d9ea5daa6286ef07db4a5a11b7707f6b17ab24
PremSaiRam <23kb1a30c0@nbkrist.org>
2025-10-10 22:24:49 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "ai-resume-helper-5d28a.firebaseapp.com", // MUST stay like this
+  apiKey: "AIzaSyBKkEfqKajj_5OouYFRROIvF6FFgeT5N8o", // <-- must match your Firebase project
+  authDomain: "ai-resume-helper-5d28a.firebaseapp.com",
   projectId: "ai-resume-helper-5d28a",
   storageBucket: "ai-resume-helper-5d28a.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  messagingSenderId: "401881371287",
+  appId: "1:401881371287:web:557cd250bd02ca4c6ecfd2",
 };
 
 const app = initializeApp(firebaseConfig);
