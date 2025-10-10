77256b8e6292207f2c945b6ecaf019c7b3d26492
PremSaiRam <23kb1a30c0@nbkrist.org>
2025-10-10 21:52:25 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // root is your frontend folder by default
   build: {
-    outDir: 'dist', // output goes to frontend/dist
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
+    outDir: "dist",
   },
 });
