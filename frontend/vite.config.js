61936a382a2cef6eae0db812f8dde0ffbc5fe623
PremSaiRam <23kb1a30c0@nbkrist.org>
2025-10-11 14:12:38 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // important: ensures correct origin for Firebase auth
-  server: {
-    port: 5173,
-    strictPort: true
+  build: {
+    outDir: "dist",
   },
-})
+});
